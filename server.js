1620a5c34c661921c9c7fe60cf76d5623d8e056a
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2015-11-02 18:31:30 -0500

    Create entry point and server it on path /

--- server.js
+++ server.js
@@ -1,13 +1,34 @@
 import express from 'express';
 import path from 'path';
 
+import webpack from 'webpack';
+import webpackDevMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+var config = require('./webpack.config');
+
+import React from 'react';
+import ReactDom from 'react-dom/server';
+import Component from './src/app/entry';
+
 let app = express();
 const port = 3000;
+
+var compiler = webpack(config)
+app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }))
+app.use(webpackHotMiddleware(compiler))
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/', (request, response) => {
-  response.send("markup");
+
+  let props = {
+    styles : "/styles.css",
+    scripts: ["/bundle.js"]
+  }
+
+  let element = React.createFactory(Component);
+  let markup = ReactDom.renderToStaticMarkup(element(props));
+  response.send(markup);
 });
 
 app.get('*', (request, response) => {
---
+++ src/app/entry.jsx
@@ -0,0 +1,40 @@
+'use strict';
+
+import React from 'react';
+import App from './components/app';
+
+export default class Entry extends React.Component {
+
+	constructor(props, context){
+		super(props, context);
+	}
+
+	render(){
+
+		let scripts = this.props.scripts.map((script, index)=>{
+			return (<script key={index} src={script}></script>)
+		});
+
+		return (<html>
+				<head>
+					<title>Walker Randolph Smith</title>
+					<meta name="description" content="Walker Randolph Smith resume." />
+					<meta name="author" content="Walker Randolph Smith" />
+					<link rel="canonical" href="http://walkerrandolphsmith.github.com/" />
+
+					<link rel="icon" type="image/png" href="profile.png" />
+					<link rel="apple-touch-icon" sizes="57x57" href="apple-icon-57x57.png" />
+					<link rel="apple-touch-icon" sizes="72x72" href="apple-icon-72x72.png" />
+					<link rel="apple-touch-icon" sizes="114x114" href="apple-icon-114x114.png" />
+					<link rel="apple-touch-icon" sizes="144x144" href="apple-icon-144x144.png" />
+          <link href={this.props.styles} rel="stylesheet" type="text/css" />
+				</head>
+				<body>
+					<div id="app">
+						<App />
+					</div>
+          {scripts}
+				</body>
+		</html>);
+	}
+}
