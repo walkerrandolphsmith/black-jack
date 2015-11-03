8e4ac03e24d08247a2d88bc7e1a7abd020ebf37c
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2015-11-02 21:10:51 -0500

    Access our state on the client - transform it back to a Immutable js collection - Pass it to redux when we instantiate new store

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -3,11 +3,27 @@ import ReactDom from 'react-dom';
 import { Router } from 'react-router';
 import createBrowserHistory from 'history/lib/createBrowserHistory';
 import routes from './../routes';
+import {createStore, combineReducers} from 'redux';
+import { Provider } from 'react-redux';
+import * as reducers from './../shared/reducers';
+import { fromJS } from 'immutable';
 
 const history = createBrowserHistory();
-const mountNode = document.getElementById('app');
 
+let initialState = window.__INITIAL_STATE__;
+Object
+  .keys(initialState)
+  .forEach(key => {
+    initialState[key] = fromJS(initialState[key]);
+  });
+
+const reducer = combineReducers(reducers);
+const store = createStore(reducer, initialState);
+
+const mountNode = document.getElementById('app');
 ReactDom.render(
-  <Router children={routes} history={history} />,
+  <Provider store={store}>
+    <Router children={routes} history={history} />
+  <Provider>,
   mountNode
 );
