68546fa6cf193fe8ad933e66e3bfca2792c90ea9
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2015-11-08 12:22:41 -0500

    Include the stylesheet link in html head

--- server.js
+++ server.js
@@ -57,6 +57,7 @@ app.use((request, response) => {
             <link rel="apple-touch-icon" sizes="72x72" href="apple-icon-72x72.png" />
             <link rel="apple-touch-icon" sizes="114x114" href="apple-icon-114x114.png" />
             <link rel="apple-touch-icon" sizes="144x144" href="apple-icon-144x144.png" />
+            <link rel="stylesheet" type="text/css" href="assets/deck.css">
           </head>
           <body>
             <div id="app">${componentHTML}</div>
