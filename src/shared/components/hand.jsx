dd069f4f75a974f3d47855e81faae2d0421bbf43
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2015-11-09 21:04:24 -0500

    Display a component for a hand of cards.

---
+++ src/shared/components/hand.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Card from './card';
+import MovesView from './moves';
+
+export default class Player extends React.Component {
+  render() {
+    const { hand, isOpponent, dispatch } = this.props;
+
+    let cards = hand.map((card, j) => {
+      return (
+        <Card key={j} id={j} suit={card.suit} rank={card.rank} />
+      );
+    });
+
+    return (
+      <div id="my-hand">
+        {cards}
+      </div>
+    )
+  }
+}
--- src/shared/components/player.jsx
+++ src/shared/components/player.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Card from './card';
+import Hand from './hand';
 import MovesView from './moves';
 
 export default class Player extends React.Component {
@@ -7,21 +7,15 @@ export default class Player extends React.Component {
     const { game, dispatch } = this.props;
 
     let player = game.players[0];
-
-    let cards = player.hand.map((card, j) => {
-      return (
-        <Card key={j} id={j} suit={card.suit} rank={card.rank} />
-      );
-    });
     let playerId = `player-${player.pid}`;
 
     return (
       <div id="player">
         <div id={playerId}>
           <div>Total: {player.total}</div>
-          <div>Hand: {cards} </div>
         </div>
         <MovesView canHit={player.canHit} hit={this.props.hit} stay={this.props.stay} />
+        <Hand hand={player.hand} isOpponent={false} />
       </div>
     )
   }
