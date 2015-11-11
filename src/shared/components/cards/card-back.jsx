c032b22bf47d8b8707840b6c27cf71386315fdb9
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2015-11-10 19:57:19 -0500

    Create a card component that shows a card face down.

---
+++ src/shared/components/cards/card-back.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class CardBack extends React.Component {
+  render() {
+    return (
+      <div className="card">
+        <div className="card-back">
+          <span className="middle_center">
+            <img src="/assets/img/back.png" />
+          </span>
+        </div>
+      </div>
+    )
+  }
+}
