60715dfe09c9fe2cc4092060540566c1a1403769
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2015-11-10 20:02:13 -0500

    Display the first card of the opponents hand face down.

--- src/shared/components/cards/hand.jsx
+++ src/shared/components/cards/hand.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import Card from './card-front';
+import CardBack from './card-back';
 
 export default class Player extends React.Component {
   render() {
     const { hand, isOpponent, dispatch } = this.props;
 
     let cards = hand.map((card, j) => {
-      return (
-        <Card key={j} id={j} suit={card.suit} rank={card.rank} />
-      );
+      return (isOpponent && j === 0) ? (<CardBack />) : (<Card key={j} id={j} suit={card.suit} rank={card.rank} />);
     });
 
     return (
