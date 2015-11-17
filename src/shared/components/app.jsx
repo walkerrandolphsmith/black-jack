824a7721395c8e434cb0e89f1a08c6af4b05f4a9
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2015-11-16 19:34:17 -0500

    Remove black jack label from root component

--- src/shared/components/app.jsx
+++ src/shared/components/app.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 export default class App extends React.Component {
   render(){
     return (<div>
-      <h1> Black Jack </h1>
       {this.props.children}
     </div>);
   }
